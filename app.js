31eed0cdd9932c65472fa7362a982a1d19e909bd
Artem Lapkin <lapkin.artem@gmail.com>
2016-11-01 09:24:47 +0200

    working clusters and domain

--- app.js
+++ app.js
@@ -8,6 +8,46 @@ app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
 
+app.use(function(req, res, next){
+// создаем домен для этого запроса
+  var domain = require('domain').create();
+// обрабатываем ошибки на этом домене
+  domain.on('error', function(err){
+    console.error('ПЕРЕХВАЧЕНА ОШИБКА ДОМЕНА\n', err.stack);
+    try {
+// Отказобезопасный останов через 5 секунд
+      setTimeout(function(){
+        console.error(' Отказобезопасный останов.');
+        process.exit(1);
+      }, 5000);
+// Отключение от кластера
+      var worker = require('cluster').worker;
+      if(worker) worker.disconnect();
+// Прекращение принятия новых запросов
+      server.close();
+      try {
+// Попытка использовать маршрутизацию
+// ошибок Express
+        next(err);
+      } catch(err){
+// Если маршрутизация ошибок Express не сработала,
+        // пробуем выдать текстовый ответ Node
+        console.error('Сбой механизма обработки ошибок ' +
+          'Express .\n', err.stack);
+        res.statusCode = 500;
+        res.setHeader('content-type', 'text/plain');
+        res.end('Ошибка сервера.');
+      }
+    } catch(err){
+      console.error('Не могу отправить ответ 500.\n', err.stack);
+    }
+  });
+// Добавляем объекты запроса и ответа в домен
+  domain.add(req);
+  domain.add(res);
+// Выполняем оставшуюся часть цепочки запроса в домене
+  domain.run(next);
+});
 
 app.set('port', process.env.PORT || 3000);
 
